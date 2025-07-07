17218f30a85aa28604e03f89bb67eca1e1699818
Phoo Pwint Wai <phoopwint.wai@kbzbank.com>
2025-07-07 14:36:53 +0630

    Last updated

--- index.js
+++ index.js
@@ -4,18 +4,6 @@ const toDoRoutes = require('./routes/todoRoutes');
 const serverless = require('serverless-http');
 const port = 8080;
 
-app.use((req, res, next) => {
-    if (typeof req.body === 'string') {
-        try {
-            req.body = JSON.parse(req.body);
-        } catch (e) {
-            console.error('Error parsing JSON:', e);
-            return res.status(400).send('Invalid JSON');
-        }
-        
-    }
-    next();
-});
 
 app.use(express.json());
 app.use('/todos', toDoRoutes);
